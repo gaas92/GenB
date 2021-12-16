fabd4aa124a9b48d375a0fcbadcbe3ab3f2b0aca
Gabriel <gaba929@gmail.com>
2021-12-15 18:35:56 -0600

    add generartion for T3 Cinvestav

--- ResonantNoProbeFilterDecayFilter/crab_B0toKs0JpsiMuMu_noProbeFilterDecayFilter_fromGen2MiniAOD_higgs.py
+++ ResonantNoProbeFilterDecayFilter/crab_B0toKs0JpsiMuMu_noProbeFilterDecayFilter_fromGen2MiniAOD_higgs.py
@@ -12,7 +12,7 @@ gen_var = '-run_cfg.py'
 step = 'PrivateMC-2018'
 nEvents = 5000
 NJOBS = 5000
-mygen = "step0-GS-"+channel+gen_var
+mygen = "step0-GS-"+channel+gen_var 
 myname = step+'-'+channel
 
 config.General.requestName = step+'-'+channel+'-'+st

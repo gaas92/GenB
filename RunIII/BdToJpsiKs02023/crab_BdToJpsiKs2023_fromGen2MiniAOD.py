83f3056fd64f3dadbe76ed40c011f20a9a4db790
gaas92 <gaba929@gmail.com>
2023-08-16 14:08:09 -0600

    working gen for resonant 2023

--- RunIII/BdToJpsiKs02023/crab_BdToJpsiKs2023_fromGen2MiniAOD.py
+++ RunIII/BdToJpsiKs02023/crab_BdToJpsiKs2023_fromGen2MiniAOD.py
@@ -33,7 +33,7 @@ config.JobType.disableAutomaticOutputCollection = True
 config.JobType.eventsPerLumi = 10000
 config.JobType.numCores = 1
 config.JobType.maxMemoryMB = 3500
-config.JobType.scriptExe = 'job_test_fromGen2MiniAOD.sh'
+config.JobType.scriptExe = 'crabjob_BdToJpsiKs2023_fromGen2MiniAOD.sh'
 #config.JobType.scriptArgs = ["0"]
 
 #config.JobType.outputFiles = ['step0-GS-'+channel+gen_frag+'-result.root']

3defacbe292b611efdcbbb37fa94fbf368bd1d59
gaas92 <gaba929@gmail.com>
2023-09-08 13:06:37 -0600

    try VSV physics model for resonant 2022 only gen

---
+++ RunIII/BdToJpsiKs02022/crab_BdToJpsiKs2022_onlyGen_SVS_NoFilter.py
@@ -0,0 +1,50 @@
+from CRABClient.UserUtilities import config
+import datetime
+import time
+
+config = config()
+
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M')
+
+channel = 'BdToKs0JpsiMuMu' 
+gen_var = 'SVS-NoFilter-run_cfg.py'
+step = 'PrivateMC-2022'
+nEvents = 5000#36554
+NJOBS = 100
+mygen = "step0-GS-"+channel+gen_var
+myname = step+'-'+channel
+
+config.General.requestName = step+'-'+channel+'-'+st
+config.General.transferOutputs = True
+config.General.transferLogs = False
+config.General.workArea = 'crab_'+step+'-'+channel+'-onlyGen'
+
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = mygen
+
+# For SIM  
+#config.JobType.inputFiles = ['step1-GENSIMRAW-'+channel+gen_var,
+#                             'step2-AODSIM-'+channel+gen_var,
+#                             'step3-MINIAODSIM-'+channel+gen_var]
+
+config.JobType.disableAutomaticOutputCollection = True
+config.JobType.eventsPerLumi = 10000
+config.JobType.numCores = 1
+config.JobType.maxMemoryMB = 3500
+config.JobType.scriptExe = 'crabjob_BdToJpsiKs2022_onlyGen_SVS_NoFilter.sh'
+#config.JobType.scriptArgs = ["0"]
+
+#config.JobType.outputFiles = ['step0-GS-'+channel+gen_frag+'-result.root']
+config.JobType.outputFiles = ['step0-GS-'+channel+'-SVS-NoFilter-result.root']
+
+config.Data.outputPrimaryDataset = myname
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = nEvents
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+#config.Data.outLFNDirBase = '/store/user/hcrottel/'
+config.Data.publication = False
+
+config.Data.outLFNDirBase = '/store/user/gayalasa/'+step+channel+'-onlyGen'+'/'
+config.Site.storageSite = 'T3_CH_CERNBOX'
---
+++ RunIII/BdToJpsiKs02022/prepare_BdToJpsiKs2022_onlyGen_SVS_NoFilter.sh
@@ -0,0 +1,60 @@
+
+#!/bin/bash
+#all steps from https://cms-pdmv.cern.ch/mcm/requests?dataset_name=B0ToJpsiK0s_JMM_BMuFilter_DGamma0_SoftQCDnonD_TuneCP5_13p6TeV-pythia8-evtgen&page=0&shown=2151677971 
+#for some reason this doesn't work
+
+#export SCRAM_ARCH=el8_amd64_gcc10
+ 
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+if [ -r CMSSW_12_4_11_patch3/src ] ; then
+  echo release CMSSW_12_4_11_patch3 already exists
+else
+  scram p CMSSW CMSSW_12_4_11_patch3
+fi
+cd CMSSW_12_4_11_patch3/src
+eval `scram runtime -sh`
+
+
+# Configuration parameters
+CHANNEL_DECAY="BdToKs0JpsiMuMu"
+step0_fragmentfile="BdToKs0JpsiMuMu_BPH-Commission22GS-SVS-NoFilter.py" 
+step0_configfile="step0-GS-${CHANNEL_DECAY}-SVS-NoFilter-run_cfg.py"
+step0_resultfile="step0-GS-${CHANNEL_DECAY}-SVS-NoFilter-result.root"
+
+# Download fragment from myGitHub
+curl -s -k https://raw.githubusercontent.com/gaas92/GenB/master/RunIII/GenFragments/$step0_fragmentfile --retry 3 --create-dirs -o Configuration/GenProduction/python/$step0_fragmentfile
+[ -s Configuration/GenProduction/python/$step0_fragmentfile ] || exit $?;
+
+# Check if fragment contais gridpack path ant that it is in cvmfs
+if grep -q "gridpacks" Configuration/GenProduction/python/$step0_fragmentfile; then
+  if ! grep -q "/cvmfs/cms.cern.ch/phys_generator/gridpacks" Configuration/GenProduction/python/$step0_fragmentfile; then
+    echo "Gridpack inside fragment is not in cvmfs."
+    exit -1
+  fi
+fi
+scram b
+cd ../..
+
+# Maximum validation duration: 57600s
+# Margin for validation duration: 30%
+# Validation duration with margin: 57600 * (1 - 0.30) = 40320s
+# Time per event for each sequence: 1.1030s
+# Threads for each sequence: 1
+# Time per event for single thread for each sequence: 1 * 1.1030s = 1.1030s
+# Which adds up to 1.1030s per event
+# Single core events that fit in validation duration: 40320s / 1.1030s = 36554
+# Produced events limit in McM is 10000
+# According to 0.0140 efficiency, validation should run 10000 / 0.0140 = 712758 events to reach the limit of 10000
+# Take the minimum of 36554 and 712758, but more than 0 -> 36554
+# It is estimated that this validation will produce: 36554 * 0.0140 = 512 events
+EVENTS=5000 #36554
+
+# cmsDriver command
+#cmsDriver.py Configuration/GenProduction/python/$step0_fragmentfile --python_filename $step0_configfile --eventcontent RAWSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --fileout file:$step0_resultfile --conditions 124X_mcRun3_2022_realistic_v12 --beamspot Realistic25ns13p6TeVEarly2022Collision --customise_commands process.source.numberEventsInLuminosityBlock="cms.untracked.uint32(2397)" --step GEN,SIM --geometry DB:Extended --era Run3 --no_exec --mc -n $EVENTS || exit $? ;
+#cmsDriver.py Configuration/GenProduction/python/$step0_fragmentfile --python_filename $step0_configfile --eventcontent RAWSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --fileout file:$step0_resultfile --conditions 125X_mcRun3_2022_realistic_v3 --beamspot Realistic25ns13p6TeVEarly2022Collision --customise_commands process.source.numberEventsInLuminosityBlock="cms.untracked.uint32(3571)" --step GEN,SIM --geometry DB:Extended --era Run3 --no_exec --mc -n $EVENTS || exit $? ;
+
+cmsDriver.py Configuration/GenProduction/python/$step0_fragmentfile --python_filename $step0_configfile --eventcontent RAWSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --fileout file:$step0_resultfile --conditions 124X_mcRun3_2022_realistic_postEE_v1 --beamspot Realistic25ns13p6TeVEarly2022Collision --customise_commands process.source.numberEventsInLuminosityBlock="cms.untracked.uint32(7127)" --step GEN,SIM --geometry DB:Extended --era Run3 --no_exec --mc -n $EVENTS || exit $? ;
+#check if necessary
+sed -i "20 a from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper \nrandSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService)\nrandSvc.populate()" $step0_configfile
+echo cmsDriver for step-0 Gen ok 
+
